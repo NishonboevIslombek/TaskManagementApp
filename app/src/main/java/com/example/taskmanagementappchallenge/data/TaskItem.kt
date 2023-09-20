7c10dc408bf1e754885e94b9fe76f203f9daf948
islombeknishonboev <nishonboevislombek@gmail.com>
2023-09-20 21:29:22 +0500

    refactor: TaskItem updated with new values

--- app/src/main/java/com/example/taskmanagementappchallenge/data/TaskItem.kt
+++ app/src/main/java/com/example/taskmanagementappchallenge/data/TaskItem.kt
@@ -3,5 +3,8 @@ package com.example.taskmanagementappchallenge.data
 data class TaskItem(
     val id: Int,
     var isCompleted: Boolean = false,
-    val title: String
+    val title: String,
+    val description: String,
+    val date: String,
+    val time: String
 )
